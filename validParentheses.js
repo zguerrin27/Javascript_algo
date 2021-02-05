9afe504eba07068ac4eda7f1806dcebb1397fc35
Zachary Guerrin <z.guerrin@gmail.com>
2021-02-05 13:50:52 -0500

    valid paren

--- validParentheses.js
+++ validParentheses.js
@@ -1,20 +1,31 @@
 var isValid = function(s) {
+    let map = {
+        '(' : ')',
+        '{' : '}',
+        '[' : ']'
+    }
     
-  var obj = {
-      ')': '(',
-      '}': '{',
-      ']': '[',
-  }
-  var stack = [];
-  
-  for (var i = 0; i < s.length; i++) {   
-      if (obj[s[i]]) {     
-          if (stack.pop() !== obj[s[i]]) {
-               return false
-          }       
-      } else stack.push(s[i]);        
-  }
-     
-  return stack.length > 0 ? false : true;
-  
+    let stack = []
+    
+    for(let i = 0; i < s.length; i++){ 
+        if(s[i] in map){              //checks keys of map. So if the current array index is an open brack..push it into the stack
+            stack.push(s[i])
+          console.log("stack", stack)
+        } else {                            // current index in array is not a key in map, or is a closed bracket.
+            let data = stack.pop()          // take the last item in stack array, top of stack item 
+            console.log("data", data)
+            console.log('map[data]', map[data])
+            console.log('s[i]', s[i])
+            console.log('============')
+            
+            if(map[data] !== s[i]) return false   //check if the the value of map[data] which would be map of open bracket..=== a closing bracket
+        }
+    }
+    if(stack.length > 0) return false  //if there are left over open brackets...its false 
+    return true
 };
+
+let str = "()[]{}"
+
+
+console.log(isValid(str))
