08a029349aa4be8cf53d8f0bdca22b044f09e666
Zachary Guerrin <z.guerrin@gmail.com>
2021-02-24 18:54:11 -0500

    refactor

--- palindromesInLargeString.js
+++ palindromesInLargeString.js
@@ -1,4 +1,4 @@
-// return number of palindrome word substrings in a large string
+// return palindrome word substrings in a large string
 // ignore  any smaller palindromes inside a larger pal
 
 // there are even and odd length pals
@@ -10,7 +10,7 @@
 
 
 // let sent = 'kjABABAtCGHHGCinAAokBABuABA' 
-//                       123         
+// pointers              123         
 // indexs of pals   2,4   2,6  4,6  8,13  16,17  20,22  24,26
 // remove           2,4 and 4,6 because they are within 2,6 range
 
@@ -23,11 +23,10 @@ function returnSubStringCount(s){
   let pals = [] 
   
   while(p2 < s.length){  // p2 to handle if the last two chars make a pal
-
     if(s[p1] === s[p3]){
-        let oddPal = buildPalindrome(s, p1, p3)    // build full length pal
-        pals = addToList(pals, oddPal)             // add to pals array if this new pal passes checks
-        pals = removeFromList(pals, oddPal)        // run the remove function incase this pal is a larger version of previous pal
+        let oddPal = buildPalindrome(s, p1, p3) // build full length pal
+        pals = addToList(pals, oddPal)          // add to pals array if this new pal passes checks
+        pals = removeFromList(pals, oddPal)     // run the remove function incase this pal is a larger version of previous pal
         p1++
         p2++
         p3++
@@ -66,7 +65,7 @@ function buildPalindrome(s, index1, index2){
   }
 }
 
-//logic for making sure we only return the full palindromes and not the sub palindromes unless it is stand alone.
+// logic for making sure we only return the full palindromes and not the sub palindromes unless it is stand alone.
 
 // input string  =     'kjABABAtCGHHGCinAAokBABuABA' 
 // pointers                      123         
@@ -74,24 +73,31 @@ function buildPalindrome(s, index1, index2){
 // remove 2,4 and 4,6 because they are within 2,6 range
 // ending pal index pairs [[2,6],[8,13],[16,17],[20,22],[24,26]]
 
-// start_index >= curr_start_index && end_index <= curr_end_index => remove existing elements in list
-// start_index <= curr_start_index && end_index >= curr_end_index => dont add current element to list
-// 2 <= 4 && 6 <= 6
-
 function removeFromList(pals, palToAdd){
-  if(pals.length <= 1) return pals
-  let temp = []
-  for(const entry of pals){
-    if(entry.index1 === palToAdd.index1 && entry.index2 === palToAdd.index2) {
-        temp.push(entry);
-    } else if(entry.index1 <= palToAdd.index1 && entry.index2 <= palToAdd.index2 && entry.index2 >= palToAdd.index1 ){
-      continue
-    } else {
-      temp.push(entry)
-    }
-  }
-  return temp
+ if(pals.length <= 1) return pals;
+   pals = pals.filter(pal => 
+     pal.index1 > palToAdd.index1 || 
+     pal.index2 > palToAdd.index2 || 
+     pal.index2 < palToAdd.index1 || 
+     (pal.index1 === palToAdd.index1 && pal.index2 === palToAdd.index2)
+   );
+ return pals;
 }
+
+// function removeFromList(pals, palToAdd){
+//   if(pals.length <= 1) return pals
+//   let temp = []
+//   for(const entry of pals){
+//     if(entry.index1 === palToAdd.index1 && entry.index2 === palToAdd.index2) {
+//         temp.push(entry);
+//     } else if(entry.index1 <= palToAdd.index1 && entry.index2 <= palToAdd.index2 && entry.index2 >= palToAdd.index1 ){
+//       continue
+//     } else {
+//       temp.push(entry)
+//     }
+//   }
+//   return temp
+// }
 
 
 // dont add a new pal to pals array if there is already a pal with a larger index range.
@@ -121,4 +127,3 @@ let sentence = "kjABABAtCGHHGCinAAokBABuABA" // "ABABA", "CGHHGC", "AA", "BAB", 
 
 
 console.log(returnSubStringCount(sentence))  // currently returns array of objects. Each obj is the palindrome string and indexs. This could just return the count, or an array of strings etc. 
-
